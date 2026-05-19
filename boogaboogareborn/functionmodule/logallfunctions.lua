1a5fcb2fd953244298d9040ef9e82216cf46ca26
herkle <69731592+decryp1@users.noreply.github.com>
2026-05-18 19:02:12 -0500

    created logallfunctions

--- boogaboogareborn/functionmodule/logallfunctions.lua
+++ boogaboogareborn/functionmodule/logallfunctions.lua
@@ -1,1 +1,24 @@
-
+--[[
+unless you add a check for what function you're spoofing, or functions to ignore, this will crash your game or cause major lag
+  ]]
+local a = require(game:GetService("ReplicatedStorage").Game.functions)
+warn(a)
+for i, v in pairs(a) do
+	print(i, v)
+	if typeof(v) == "function" then
+		local o = v
+		hookfunction(v, function(...)
+			args = {...} -- store original arguments without forcing structure
+			print("{{" .. i .. "}}" .. " was fired with " .. #args .. " arguments")
+      		for a, s in pairs(args) do
+          		print(a ~= nil and a or "key is nil, i forgot whether or not pairs skips nil keys", s ~= nil and s or "value is nil")
+          		if typeof(s) == "table" then
+            		for d, f in pairs(s) do
+              			warn(d ~= nil and d or "key is nil", f ~= nil and f or "value is nil")
+            		end
+          		end
+      		end
+			return o(args); -- return the original function call after the logger with the original arguments
+		end)
+	end
+end
