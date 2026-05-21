091328fbf536e05253446898bfa76d0cf8db8865
herkle <69731592+decryp1@users.noreply.github.com>
2026-05-21 15:16:46 -0500

    canplace logger

---
+++ boogaboogareborn/gameutil/canplace_logger.lua
@@ -0,0 +1,18 @@
+local s = require(game:GetService("ReplicatedStorage").Modules.GameUtil)
+local tray = s.canPlace
+local argss = {}
+hookfunction(s.canPlace, function(...)
+	local args = {...}
+	for i, v in pairs(args) do
+		--print(i, v)
+		table.insert(argss, v)
+	end
+	return tray(table.unpack(args))
+end)
+
+wait(6)
+
+for i = 1, 40 do
+	if i == 1 then print("total arguments logged:", #argss); end
+	print(argss[i])
+end
