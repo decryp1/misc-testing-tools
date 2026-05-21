b22417bf94fb6273c9b659bce8ca6daf89f1f903
herkle <69731592+decryp1@users.noreply.github.com>
2026-05-21 15:38:32 -0500

    infinitepickuprange

---
+++ boogaboogareborn/pickupmodule/infinitepickuprange_realrael.lua
@@ -0,0 +1,13 @@
+-- doesnt actually work loser
+local a = require(game:GetService("ReplicatedStorage").Game.pickup)
+--a.Try = function() return true end
+of = a.Try
+hookfunction(a.Try, function(...)
+	print(...)
+	return true
+end)
+--[[ -- run this to remove the hook
+if isfunctionhooked(a.Try) then
+	restorefunction(a.Try)
+end
+]]
