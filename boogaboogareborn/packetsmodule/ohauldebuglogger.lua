c9b1fda309be19c1b5f5cbc5ee20ed7d3226a527
herkle <69731592+decryp1@users.noreply.github.com>
2026-05-22 19:08:08 -0500

    something

---
+++ boogaboogareborn/packetsmodule/ohauldebuglogger.lua
@@ -0,0 +1,10 @@
+for i, v in pairs(getrenv().debug) do
+	print(i, v)
+	if not i == ("profilebegin" or "profileend") then
+		local o; o = hookfunction(v, newcclosure(function(...)
+			print(i, "was called")
+			return o(...)
+		end))
+	end
+end
+-- does nothing
