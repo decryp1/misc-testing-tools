d296fe3f6ca5d91aa56dd7f3ccb9471345a59230
herkle <69731592+decryp1@users.noreply.github.com>
2026-06-22 10:20:32 -0500

    utils

---
+++ utils.lua
@@ -0,0 +1,66 @@
+utils = {
+	function createherklefiles()
+		isfolder = isfolder or is_folder or checkfolder or check_folder or nil;
+		makefolder = makefolder or make_folder or folder or nil;
+		writefile = writefile or write_file or nil;
+		if not isfolder("herkleglobals") then
+			makefolder("herkleglobals");
+			writefile("herkleglobals/executions.txt", "1");
+			writefile("herkleglobals/invited.txt", "false");
+		end
+	end
+
+	function addexecution()
+		createherklefiles()
+		local num=tonumber(readfile("herkleglobals/executions.txt"))
+		if num then
+			writefile("herkleglobals/executions.txt",tostring(num + 1))
+		end
+	end
+
+	function invitetodiscord()
+		createherklefiles()
+		if readfile("herkleglobals/invited.txt") == ("false" or nil) then
+			if not request then notifyy("your executor does not support request! here is the discord: https://dsc.gg/herkle", 5, Color3.fromRGB(200, 50, 50))
+				return
+			end
+			request({
+				Url = "http://127.0.0.1:6463/rpc?v=1", Method = "POST",
+				Headers = {["Content-Type"] = "application/json", ["Origin"] = "https://discord.com"},
+				Body = game:GetService("HttpService"):JSONEncode({cmd = "INVITE_BROWSER", args = {code = "uFYfMQGzk8"}, nonce = game:GetService("HttpService"):GenerateGUID(false)})
+			})
+			writefile("herkleglobals/invited.txt", "true")
+		end
+	end
+
+	function getglobals(which: string): string
+		createherklefiles()
+		return which == "executions" and readfile("herkleglobals/executions.txt") or readfile("herkleglobals/invited.txt")
+	end
+
+	function removeherklenoo(shouldwipeconfigs: boolean)
+    	for i,v in listfiles("") do
+      		if tostring(v):lower():find("herkle") then
+        		if shouldwipeconfigs then
+          			if isfolder(v) then
+            			delfolder(v)
+          			elseif isfile(v) then
+            			delfile(v)
+					end
+					print("wiped configs")
+				end 
+				if tostring(v) == "herkleglobals" and isfolder(v) then
+					print("removed herkleglobals")
+          			delfolder(v)
+        		end
+      		end
+		end
+	end
+
+}
+return utils
+--[[print(getglobals("executions"), getglobals("invited"))
+utils.invitetodiscord()
+utils.addexecution()
+utils.removeherklenoo(false)
+]]
