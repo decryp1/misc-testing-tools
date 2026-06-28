94588cf8b4cd9a84a0565f03f611c0ffc6a8f7b1
herkle <69731592+decryp1@users.noreply.github.com>
2026-06-28 13:38:11 -0500

    slight fix

--- boogaboogareborn/slashmodule/tool_hitboxextender.lua
+++ boogaboogareborn/slashmodule/tool_hitboxextender.lua
@@ -1,13 +1,18 @@
 if not _G.size then
 	_G.size = Vector3.new(50, 50, 50)
 end
-local o; o = hookmetamethod(game, "__namecall", newcclosure(function(self, cf, size, args)
+local o; o = hookmetamethod(game, "__namecall", newcclosure(function(self, ...)
 	if self == workspace and getnamecallmethod() == "GetPartBoundsInBox" then
-		print(self, cf,size,args)
-		size = _G.size or Vector3.new(50, 50, 50)
-		if _G.changecf then	
-			cf = _G.cf or game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+		local args = {...}
+		if typeof(args[1]) == "CFrame" and typeof(args[2]) == "Vector3" then
+			--print(self, cf,size,args)
+			--print(getcallingscript())
+			args[2] = _G.size or Vector3.new(50, 50, 50)
+			if _G.changecf then
+				args[1] = _G.cf or game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+			end
 		end
+		return o(self, unpack(args))
 	end
-	return o(self, cf,size,args)
+	return o(self, ...)
 end))
