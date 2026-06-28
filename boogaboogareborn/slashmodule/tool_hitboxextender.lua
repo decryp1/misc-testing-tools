9cc2c2a65b3c4e6b4b73d08f39e01ea8fbd82709
herkle <69731592+decryp1@users.noreply.github.com>
2026-06-28 13:23:05 -0500

    ehhh

--- boogaboogareborn/slashmodule/tool_hitboxextender.lua
+++ boogaboogareborn/slashmodule/tool_hitboxextender.lua
@@ -1,10 +1,10 @@
 if not _G.size then
-	_G.size = Vector3.new(15, 15, 15)
+	_G.size = Vector3.new(50, 50, 50)
 end
 local o; o = hookmetamethod(game, "__namecall", newcclosure(function(self, cf, size, args)
 	if self == workspace and getnamecallmethod() == "GetPartBoundsInBox" then
 		print(self, cf,size,args)
-		size = _G.size or Vector3.new(15, 15, 15)
+		size = _G.size or Vector3.new(50, 50, 50)
 		if _G.changecf then	
 			cf = _G.cf or game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
 		end
