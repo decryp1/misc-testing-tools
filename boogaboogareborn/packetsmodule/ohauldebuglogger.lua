62a2cd964f5218cb8e68e67065b89413b87c1982
herkle <69731592+decryp1@users.noreply.github.com>
2026-07-10 17:54:36 -0500

    debug logger does something now!!!!!!!

--- boogaboogareborn/packetsmodule/ohauldebuglogger.lua
+++ boogaboogareborn/packetsmodule/ohauldebuglogger.lua
@@ -7,4 +7,16 @@ for i, v in pairs(getrenv().debug) do
 		end))
 	end
 end
--- does nothing
+
+--[[
+for i, v in pairs(getrenv().debug) do
+	print(i, v)
+	if not (i == "profilebegin" or i == "profileend" or i == "traceback") then -- traceback for insight, wow recursion wow
+		local o; o = hookfunction(v, newcclosure(function(...)
+			print(i, "was called");
+			print(debug.traceback())
+			return o(...)
+		end))
+	end
+end
+]]
