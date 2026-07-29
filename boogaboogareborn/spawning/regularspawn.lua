e6ab526a4c308266ddd40b888e8f9bff3cceeb82
herkle <69731592+decryp1@users.noreply.github.com>
2026-07-28 20:46:52 -0500

    added regular spawning

---
+++ boogaboogareborn/spawning/regularspawn.lua
@@ -0,0 +1,2 @@
+getconnections(game:GetService("Players").LocalPlayer.PlayerGui.SpawnGui.Customization.PlayButton.Activated)[1].Function()
+--i didnt try on this there are probably other better methods but i know this one works so here u go
