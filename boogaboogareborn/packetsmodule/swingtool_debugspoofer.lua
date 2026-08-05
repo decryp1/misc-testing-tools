8ac3cf1952336830651714796a516fcc9c5e30ac
herkle <69731592+decryp1@users.noreply.github.com>
2026-08-04 20:53:38 -0500

    so ugly

--- boogaboogareborn/packetsmodule/swingtool_debugspoofer.lua
+++ boogaboogareborn/packetsmodule/swingtool_debugspoofer.lua
@@ -1,15 +1,13 @@
 --idk if this works but i had an old info bypass and figured thats what booga used to use for packet traceback so here it is
 -- i hate you sully
-local oldinfo = debug.info
-local a = require(game:GetService("ReplicatedStorage").Game.tool.Slash)
-debug.info = newcclosure(function(level, field)
+local oldinfo = debug.info; debug.info = newcclosure(function(level, field)
     if not checkcaller() then
         if field == "s" then
             return 'game:GetService("ReplicatedStorage").Game.tool.Slash'
         elseif field == "n" then
             return ""
         elseif field == "f" then
-            return a
+            return require(game:GetService("ReplicatedStorage").Game.tool.Slash)
         end
     end
     return oldinfo(level, field)
