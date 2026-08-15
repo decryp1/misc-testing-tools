65ede0f02eb39db5df49cf74211ad641ca940264
herkle <69731592+decryp1@users.noreply.github.com>
2026-08-14 21:31:00 -0500

    upd

--- boogaboogareborn/playerscripts/misc/disablemodtrollingstuff.lua
+++ boogaboogareborn/playerscripts/misc/disablemodtrollingstuff.lua
@@ -1,26 +1,33 @@
 --[[
-i'm pretty sure moderators have like little trolling commands or tools, and they stun you, flashbang you, or shake ur screen
-i would assume that's not all they do, and i don't even know if it's used only by moderators, but idk
-shake is the only normal-ish thing, like from meteors
+not actually mod commands, basically just a flashbang and fling disabler
 ]]
 
-todisable = {
-	["flashbang"] = true,
-	["platformstand"] = true, -- was gonna use this but i never did
-	["shake"] = true;
-};
 plr = game.Players.LocalPlayer
 chr = plr.Character or plr.CharacterAdded:Wait()
 hum = chr:FindFirstChild("Humanoid") or nil
 s = chr:FindFirstChild("Misc") or nil
+function run()
+    for i, v in pairs(getconnections(chr.ChildAdded)) do
+        print(i, v)
+        local ol = v.Function;
+        local found = false
+        if not found and #debug.getupvalues(ol) == 4 and typeof(debug.getupvalue(ol, 4)) == "function" then
+            print("found")
+            v:Disable()
+            found = true
+            break
+        end
+    end
+end
 plr.CharacterAdded:Connect(function(chrr)
 	repeat wait(.1) until #chr:GetChildren() > 5 or chr:FindFirstChild("Humanoid").Health > 0
 	chr = chrr
 	hum = chr:WaitForChild("Humanoid")
-	s = chr:FindFirstChild("Misc") or nil
+	s = chr:FindFirstChild("Misc") or nil; task.wait()
+    run()
 end)
 
-if chr and s then
+--[[if chr and s then
 	for i, v in pairs(getconnections(chr.ChildAdded)) do
 		print(i, v)
 		local ol = v.Function;
@@ -32,7 +39,7 @@ if chr and s then
 			break
 		end
 	end	
-		--[[s.Enabled = false
+		s.Enabled = false
 		for i, v in pairs(getconnections(chr.ChildAdded)) do
 		local ol = v.Function
 		hookfunction(v.Function, function(...)
@@ -45,5 +52,5 @@ if chr and s then
 			end -- idk if any of that above works, i never tested it but with some revivisions it might
 			return ol(args)
 		end)
-	end]]
-end
+	end
+end]]
